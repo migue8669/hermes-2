c2708ce058bfb7d700f920d3369b377a2f75764e
migue8669 <miguelramirez8669@gmail.com>
2020-11-04 20:00:27 -0500

    se realizan cambios finales para hermes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+    apiKey: "AIzaSyChNrQzSU64kOmx_khRUyVMkKbgA6Ary7E",
+    authDomain: "hermesbd-8fbb1.firebaseapp.com",
+    databaseURL: "https://hermesbd-8fbb1.firebaseio.com",
+    projectId: "hermesbd-8fbb1",
+    storageBucket: "hermesbd-8fbb1.appspot.com",
+    messagingSenderId: "33677417598",
+    appId: "1:33677417598:web:ae52557589c33786ee0c2f",
+    measurementId: "G-148X56LH5K"
+  }
 };
